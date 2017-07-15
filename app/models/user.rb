acc8cab1c0c6ad310cff7d1873513268495df340
Calvin <xcalvin.zhao@gmail.com>
2017-07-15 22:46:05 +0800

    after merge

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+        #  def admin?
+        #    email == 'xcalvin.zhao@gmail.com'
+        #  end
+
+         def admin?
+           is_admin
+         end
+
+  has_many :resumes
+end
